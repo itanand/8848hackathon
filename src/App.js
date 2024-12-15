bf72631ad835928074bf7d1d6b5977a60051b0e4
Anand Mohan <anand@8848digital.com>
2024-12-15 16:53:40 +0530

    Production Push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-import HeroSection from './components/HeroSection';
-import AboutHackathon from './components/AboutHackathon';
-import Themes from './components/Themes';
-import Rules from './components/Rules';
-import Footer from './components/Footer';
-import JudgingCriteria from './components/JudgingCriteria';
-import Tools from './components/Tools';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import HeroSection from "./components/HeroSection";
+import AboutHackathon from "./components/AboutHackathon";
+import Themes from "./components/Themes";
+import Rules from "./components/Rules";
+import Footer from "./components/Footer";
+import JudgingCriteria from "./components/JudgingCriteria";
+// import Tools from "./components/Tools";
+import ProjectIdeas from "./components/ProjectIdeas"; // Import the new Projects Ideas page
 
 function App() {
   return (
-    <div>
-      <HeroSection />
-      <AboutHackathon />
-      <Themes />
-      <Rules />
-      {/* <Tools /> */}
-      <JudgingCriteria />
-      <Footer />
-    </div>
+    <Router>
+      <Routes>
+        {/* Define routes for different pages */}
+        <Route
+          path="/"
+          element={
+            <div>
+              <HeroSection />
+              <AboutHackathon />
+              <Themes />
+              <Rules />
+              <JudgingCriteria />
+              <Footer />
+            </div>
+          }
+        />
+        <Route path="/projects-ideas" element={<ProjectIdeas />} />
+      </Routes>
+    </Router>
   );
 }
 
